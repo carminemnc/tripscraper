3219260fa2287a3fdb6ca89e71c34502e5fa24e7
Carmine <carmine.mnc@gmail.com>
2023-07-25 21:56:39 +0200

    new_scraper

--- scraper.py
+++ scraper.py
@@ -1,11 +1,3 @@
-from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 import streamlit as st
 import pandas as pd,time
 
@@ -16,66 +8,74 @@ def hotelTripScraper(url,pages):
     df = pd.DataFrame(columns=['review_title','review_rating','review_date','review_body','user_contributions'])
     
     # ChromeDriver Options
-    
-    @st.cache_resource
-    def get_driver():
-        return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
-    options = Options()
-    options.add_argument('--disable-gpu')
-    options.add_argument('--headless')
-
-    brwsr = get_driver()
-    
-    # get URL
-    brwsr.get(url)
-    # wait for DOM
-    time.sleep(2)
-    
-    # handling cookies
-    brwsr.find_element(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]').click()
-    
-    for page in range(0,pages):
+    with st.echo():
+        
+        from selenium import webdriver
+        from selenium.webdriver.chrome.options import Options
+        from selenium.webdriver.chrome.service import Service
+        from webdriver_manager.chrome import ChromeDriverManager  
+        from selenium.webdriver.common.by import By
         
         
-        for i in range(3,13):
-            
-            # review object
-            review = brwsr.find_element(By.XPATH,f'/html/body/div[2]/div[2]/div[2]/div[9]/div/div[1]/div[1]/div/div/div[3]/div[{i}]')
-            
-            # review title
-            title = review.find_element(By.XPATH,".//div[contains(@data-test-target, 'review-title')]").text
-            
-            # review rating
-            rating = review.find_element(By.XPATH,".//span[contains(@class, 'ui_bubble_rating bubble_')]").get_attribute("class").split("_")[3]
-            
-            
-            # review date
-            date = review.find_element(By.XPATH,".//span[contains(@class, 'teHYY _R Me S4 H3')]").text
-            
-            # expand review if it's needed
-            try:
-                review.find_element(By.XPATH,".//div[contains(@data-test-target, 'expand-review')]").click()
-            except:
-                pass
-            finally:
-                # review body
-                body = review.find_element(By.XPATH,".//span[@class='QewHA H4 _a']").text
-            
-            
-            user_contributions = review.find_element(By.XPATH,".//span[contains(@class, 'yRNgz')]").text
-            
-            # to a dataframe
-            review_objects = [title,int(rating),date,body,user_contributions] 
-            
-            df.loc[len(df)] = review_objects
-            
-            #print(len(df))
-    
+        @st.cache_resource
+        def get_driver():
+            return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-        brwsr.find_element(By.XPATH,'.//a[@class="ui_button nav next primary "]').click()
+        options = Options()
+        options.add_argument('--disable-gpu')
+        options.add_argument('--headless')
+
+        brwsr = get_driver()
         
-        time.sleep(3)
+        # get URL
+        brwsr.get(url)
+        # wait for DOM
+        time.sleep(2)
+    
+        # handling cookies
+        brwsr.find_element(By.XPATH,'//*[@id="onetrust-accept-btn-handler"]').click()
+        
+        for page in range(0,pages):
+            
+            
+            for i in range(3,13):
+                
+                # review object
+                review = brwsr.find_element(By.XPATH,f'/html/body/div[2]/div[2]/div[2]/div[9]/div/div[1]/div[1]/div/div/div[3]/div[{i}]')
+                
+                # review title
+                title = review.find_element(By.XPATH,".//div[contains(@data-test-target, 'review-title')]").text
+                
+                # review rating
+                rating = review.find_element(By.XPATH,".//span[contains(@class, 'ui_bubble_rating bubble_')]").get_attribute("class").split("_")[3]
+                
+                
+                # review date
+                date = review.find_element(By.XPATH,".//span[contains(@class, 'teHYY _R Me S4 H3')]").text
+                
+                # expand review if it's needed
+                try:
+                    review.find_element(By.XPATH,".//div[contains(@data-test-target, 'expand-review')]").click()
+                except:
+                    pass
+                finally:
+                    # review body
+                    body = review.find_element(By.XPATH,".//span[@class='QewHA H4 _a']").text
+                
+                
+                user_contributions = review.find_element(By.XPATH,".//span[contains(@class, 'yRNgz')]").text
+                
+                # to a dataframe
+                review_objects = [title,int(rating),date,body,user_contributions] 
+                
+                df.loc[len(df)] = review_objects
+                
+                #print(len(df))
+        
+
+            brwsr.find_element(By.XPATH,'.//a[@class="ui_button nav next primary "]').click()
+            
+            time.sleep(3)
             
 
     
